0ab150c579c47dcd6e675f28b3189977f794a85b
Amaury Miguel Caballero <62359438+LarryKapija@users.noreply.github.com>
2024-03-25 00:48:26 -0400

    feat: create the getNowPlayingMovies request

--- MoviesApp/app/src/main/java/com/larrykapija/moviesapp/network/api/TmdbApiService.kt
+++ MoviesApp/app/src/main/java/com/larrykapija/moviesapp/network/api/TmdbApiService.kt
@@ -1,11 +1,24 @@
 package com.larrykapija.moviesapp.network.api
 
+import com.larrykapija.moviesapp.network.response.NowPlayingResponse
 import com.larrykapija.moviesapp.network.response.PopularMoviesResponse
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.Query
 
 interface TmdbApiService {
     @GET("movie/popular")
-    fun getPopularMovies(@Query("api_key") apiKey: String): Call<PopularMoviesResponse>
+    fun getPopularMovies(
+        @Header("Authorization") accessToken: String,
+        @Query("language") language: String = "en-US",
+        @Query("page") page: Int = 1
+    ): Call<PopularMoviesResponse>
+
+    @GET("movie/now_playing")
+    fun getNowPlayingMovies(
+        @Header("Authorization") accessToken: String,
+        @Query("language") language: String = "en-US",
+        @Query("page") page: Int = 1
+    ): Call<NowPlayingResponse>
 }
