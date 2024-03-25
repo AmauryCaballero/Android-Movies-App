dca399825140f9326f6104f98c7e4b0edfdc2ff4
Amaury Miguel Caballero <62359438+LarryKapija@users.noreply.github.com>
2024-03-24 23:39:15 -0400

    feat: movie
    Movie model used in the MovieResponse and for details

--- MoviesApp/app/src/main/java/com/larrykapija/moviesapp/network/response/MovieResponse.kt
+++ MoviesApp/app/src/main/java/com/larrykapija/moviesapp/network/response/MovieResponse.kt
@@ -8,6 +8,13 @@ data class MovieResponse(
     val total_pages: Int
 )
 
+// Movie model used in the MovieResponse and for details
 data class Movie(
     val id: Int,
+    val title: String,
+    val overview: String,
+    val poster_path: String?,
+    val backdrop_path: String?,
+    val vote_average: Double,
+    val release_date: String?
 )
