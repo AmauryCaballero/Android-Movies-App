4b3ae0cf9cb9a3f4a15cea8f22035ff661ffaf5c
Amaury Miguel Caballero <62359438+LarryKapija@users.noreply.github.com>
2024-03-25 02:33:01 -0400

    theme: fonts configuration

--- MoviesApp/app/src/main/java/com/larrykapija/moviesapp/ui/theme/Type.kt
+++ MoviesApp/app/src/main/java/com/larrykapija/moviesapp/ui/theme/Type.kt
@@ -2,33 +2,41 @@ package com.larrykapija.moviesapp.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.larrykapija.moviesapp.R
 
 // Set of Material typography styles to start with
+
+val JostFontFamily = FontFamily(
+    Font(R.font.jost_bold, FontWeight.Bold),
+    Font(R.font.jost_medium, FontWeight.Medium),
+    Font(R.font.jost_light, FontWeight.Light)
+)
+
+
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+
+    titleLarge = TextStyle(
+        fontFamily = JostFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 24.sp,
+        letterSpacing = 0.5.sp,
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = JostFontFamily,
+        fontWeight = FontWeight.Medium,
         fontSize = 16.sp,
         lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        letterSpacing = 0.5.sp,
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+        fontFamily = JostFontFamily,
+        fontWeight = FontWeight.Light,
+        fontSize = 14.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp,
+    ),
 )
