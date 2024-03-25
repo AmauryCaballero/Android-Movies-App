2583a3f205fed8d9d98b9e53c2f5afaf5da09880
Amaury Miguel Caballero <62359438+LarryKapija@users.noreply.github.com>
2024-03-25 00:32:38 -0400

    feat: create NetworkModule

---
+++ MoviesApp/app/src/main/java/com/larrykapija/moviesapp/network/NetworkModule.kt
@@ -0,0 +1,48 @@
+package com.larrykapija.moviesapp.network
+
+import com.larrykapija.moviesapp.BuildConfig
+import com.larrykapija.moviesapp.network.api.TmdbApiService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.BODY
+            })
+            .addInterceptor { chain ->
+                val request = chain.request().newBuilder()
+                    .addHeader("Authorization", "Bearer ${BuildConfig.TMDB_API_ACCESS_KEY}")
+                    .build()
+                chain.proceed(request)
+            }
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit =
+        Retrofit.Builder()
+            .baseUrl("https://api.themoviedb.org/3/")
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+    @Singleton
+    @Provides
+    fun provideTmdbApiService(retrofit: Retrofit): TmdbApiService =
+        retrofit.create(TmdbApiService::class.java)
+}
