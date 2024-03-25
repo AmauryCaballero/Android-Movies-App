25ef089a2ecbb458b1a07dcd98b18fdaf5d8a6cc
Amaury Miguel Caballero <62359438+LarryKapija@users.noreply.github.com>
2024-03-25 00:36:53 -0400

    feat: provide gson on network service

--- MoviesApp/app/src/main/java/com/larrykapija/moviesapp/network/NetworkModule.kt
+++ MoviesApp/app/src/main/java/com/larrykapija/moviesapp/network/NetworkModule.kt
@@ -1,5 +1,8 @@
 package com.larrykapija.moviesapp.network
 
+import com.google.gson.FieldNamingPolicy
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import com.larrykapija.moviesapp.BuildConfig
 import com.larrykapija.moviesapp.network.api.TmdbApiService
 import dagger.Module
@@ -18,31 +21,34 @@ object NetworkModule {
 
     @Provides
     @Singleton
-    fun provideOkHttpClient(): OkHttpClient {
-        return OkHttpClient.Builder()
-            .addInterceptor(HttpLoggingInterceptor().apply {
-                level = HttpLoggingInterceptor.Level.BODY
-            })
-            .addInterceptor { chain ->
-                val request = chain.request().newBuilder()
-                    .addHeader("Authorization", "Bearer ${BuildConfig.TMDB_API_ACCESS_KEY}")
-                    .build()
-                chain.proceed(request)
-            }
-            .build()
-    }
+    fun provideGson(): Gson = GsonBuilder()
+        .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+        .create()
 
-    @Singleton
     @Provides
-    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit =
-        Retrofit.Builder()
-            .baseUrl("https://api.themoviedb.org/3/")
-            .client(okHttpClient)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient = OkHttpClient.Builder()
+        .addInterceptor(HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        })
+        .addInterceptor { chain ->
+            val request = chain.request().newBuilder()
+                .addHeader("Authorization", "Bearer ${BuildConfig.TMDB_API_ACCESS_KEY}")
+                .build()
+            chain.proceed(request)
+        }
+        .build()
 
-    @Singleton
     @Provides
+    @Singleton
+    fun provideRetrofit(gson: Gson, okHttpClient: OkHttpClient): Retrofit = Retrofit.Builder()
+        .baseUrl("https://api.themoviedb.org/3/")
+        .client(okHttpClient)
+        .addConverterFactory(GsonConverterFactory.create(gson))
+        .build()
+
+    @Provides
+    @Singleton
     fun provideTmdbApiService(retrofit: Retrofit): TmdbApiService =
         retrofit.create(TmdbApiService::class.java)
 }
