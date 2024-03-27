790d743459885d6099ef831449b865e3acc00674
Amaury Miguel Caballero <62359438+LarryKapija@users.noreply.github.com>
2024-03-27 06:27:57 -0400

    feat: adding transition on background images.

--- MoviesApp/app/src/main/java/com/larrykapija/moviesapp/ui/screens/home/components/BackgroundImage.kt
+++ MoviesApp/app/src/main/java/com/larrykapija/moviesapp/ui/screens/home/components/BackgroundImage.kt
@@ -1,24 +1,56 @@
 package com.larrykapija.moviesapp.ui.screens.home.components
 
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import coil.compose.AsyncImage
+import kotlinx.coroutines.delay
 
 @Composable
 fun BackgroundImage(imageUrl: String) {
+    val currentImageUrl = remember { mutableStateOf(imageUrl) }
+    val previousImageUrl = remember { mutableStateOf(imageUrl) }
+    val opacity = remember { Animatable(0f) }
 
+    LaunchedEffect(imageUrl) {
+        if (imageUrl != currentImageUrl.value) {
+            previousImageUrl.value = currentImageUrl.value
+            currentImageUrl.value = imageUrl
+            opacity.snapTo(0f)
+            opacity.animateTo(1f, animationSpec = tween(durationMillis = 1000))
+        }
+    }
     Box(modifier = Modifier.fillMaxSize()) {
         AsyncImage(
-            model = imageUrl,
-            contentDescription = "home page background image",
+            model = previousImageUrl.value,
+            contentDescription = "old background image",
             modifier = Modifier
                 .matchParentSize(),
+            contentScale = ContentScale.Crop
+        )
+
+        // New image
+        AsyncImage(
+            model = currentImageUrl.value,
+            contentDescription = "new background image",
+            modifier = Modifier
+                .matchParentSize()
+                .graphicsLayer { alpha = opacity.value },
             contentScale = ContentScale.Crop
         )
 
