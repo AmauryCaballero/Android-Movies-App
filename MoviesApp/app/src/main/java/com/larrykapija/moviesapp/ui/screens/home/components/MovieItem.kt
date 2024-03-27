7e97e54aa5f2e1df2371c0ccab0b6112395f1768
Amaury Miguel Caballero <62359438+LarryKapija@users.noreply.github.com>
2024-03-27 04:11:25 -0400

    feat: animating the movie item

--- MoviesApp/app/src/main/java/com/larrykapija/moviesapp/ui/screens/home/components/MovieItem.kt
+++ MoviesApp/app/src/main/java/com/larrykapija/moviesapp/ui/screens/home/components/MovieItem.kt
@@ -1,25 +1,29 @@
 package com.larrykapija.moviesapp.ui.screens.home.components
 
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.larrykapija.moviesapp.network.response.Movie
 
-
 @Composable
-internal fun MovieItem(movie: Movie, index: Int, focusedItemIndex: Int) {
+fun MovieItem(movie: Movie, index: Int, focusedItemIndex: Int) {
     val isFocused = index == focusedItemIndex
-    val rotationDegrees = when {
+
+    val rotationDegrees by animateFloatAsState(targetValue = when {
         index < focusedItemIndex -> -10f
         index > focusedItemIndex -> 10f
         else -> 0f
-    }
-    val alpha = if (isFocused) 1f else 0.7f
-    val scale = if (isFocused) 1f else 0.75f
+    }, label = "movie item rotation animation"
+    )
+
+    val alpha by animateFloatAsState(targetValue = if (isFocused) 1f else 0.7f, label = "movie item alpha animation")
+    val scale by animateFloatAsState(targetValue = if (isFocused) 1f else 0.75f, label = "movie item scale animation")
 
     AsyncImage(
         model = "https://image.tmdb.org/t/p/w500${movie.poster_path}",
