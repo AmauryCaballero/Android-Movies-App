76fdefabce63a4f129a396fe86f276710cb88910
Amaury Miguel Caballero <62359438+LarryKapija@users.noreply.github.com>
2024-03-27 04:08:08 -0400

    feat: implementing horizontal pager for movie items

--- MoviesApp/app/src/main/java/com/larrykapija/moviesapp/ui/screens/home/HomePage.kt
+++ MoviesApp/app/src/main/java/com/larrykapija/moviesapp/ui/screens/home/HomePage.kt
@@ -1,23 +1,41 @@
 package com.larrykapija.moviesapp.ui.screens.home
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PageSize
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModelStoreOwner
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.larrykapija.moviesapp.ui.viewmodel.HomePageViewModel
+import androidx.compose.runtime.livedata.observeAsState
+import com.larrykapija.moviesapp.ui.screens.home.components.MovieItem
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun HomePage(
     innerPadding: PaddingValues,
     viewModel: HomePageViewModel = viewModel(LocalContext.current as ViewModelStoreOwner)
 ) {
+    val movies = viewModel.popularMovies.observeAsState().value?.results ?: listOf()
+    val pagerState = rememberPagerState(pageCount = {
+        movies.size
+    })
+
     Box(modifier = Modifier.padding(innerPadding), contentAlignment = Alignment.Center) {
-        Text(text = viewModel.greetings)
+        HorizontalPager(
+            state = pagerState,
+            pageSize = PageSize.Fixed(200.dp),
+            contentPadding = PaddingValues(start = 100.dp)
+        ) { page ->
+            MovieItem(movie = movies[page], index = page, focusedItemIndex = pagerState.currentPage)
+        }
     }
 }
