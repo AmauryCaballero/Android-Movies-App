838b484ae9e742679fdaec0e8735ad946c67bbe1
Amaury Miguel Caballero <62359438+LarryKapija@users.noreply.github.com>
2024-03-28 07:55:33 -0400

    feat: create search state

--- MoviesApp/app/src/main/java/com/larrykapija/moviesapp/viewmodel/SearchPageViewModel.kt
+++ MoviesApp/app/src/main/java/com/larrykapija/moviesapp/viewmodel/SearchPageViewModel.kt
@@ -1,7 +1,10 @@
 package com.larrykapija.moviesapp.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.larrykapija.moviesapp.network.api.TmdbApiService
+import com.larrykapija.moviesapp.network.response.MovieDetails
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
@@ -10,4 +13,18 @@ class SearchPageViewModel @Inject constructor(
     private val tmdbApiService: TmdbApiService
 ) : ViewModel() {
 
+    private val _uiState = MutableLiveData<SearchState>()
+    val uiState: LiveData<SearchState> = _uiState
+
+    init {
+        _uiState.value = SearchState.Empty
+    }
+
+}
+
+sealed class SearchState {
+    object Empty : SearchState()
+    object Loading : SearchState()
+    data class Success(val movies: MovieDetails) : SearchState()
+    data class Error(val message: String) : SearchState()
 }
