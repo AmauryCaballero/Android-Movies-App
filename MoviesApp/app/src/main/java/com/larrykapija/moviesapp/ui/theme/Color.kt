18c5354970951652faf7fef36cb974b79f98d440
Amaury Miguel Caballero <62359438+LarryKapija@users.noreply.github.com>
2024-03-29 18:46:17 -0400

    Update Color.kt

--- MoviesApp/app/src/main/java/com/larrykapija/moviesapp/ui/theme/Color.kt
+++ MoviesApp/app/src/main/java/com/larrykapija/moviesapp/ui/theme/Color.kt
@@ -8,7 +8,7 @@ val SecondaryDark = Color(0xFF1C1C1C)
 val SecondaryLight = Color(0xFFE3E3E3)
 
 val TertiaryDark = Color(0xB2616161)
-val TertiaryLight = Color(0xFF2C2C2C)
+val TertiaryLight = Color(0xFFBDBDBD)
 
 val TextColorDark = Color(0xFFFFFFFF)
 val TextColorLight = Color(0xFF161616)
